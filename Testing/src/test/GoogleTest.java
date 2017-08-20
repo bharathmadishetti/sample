18d01388c95ce3a13577d9f080958c6e12c96a0f
Felicity M <oatstesting@gmail.com>
2017-08-20 21:01:47 +0530

    Changed the URL to flipkart

--- Testing/src/test/GoogleTest.java
+++ Testing/src/test/GoogleTest.java
@@ -11,7 +11,7 @@ public class GoogleTest {
 		 WebDriver driver;
 		System.setProperty("webdriver.gecko.driver", "browsers/geckodriver.exe");
         driver = new FirefoxDriver();
-        driver.get("http://www.facebook.com");
+        driver.get("http://www.flipkart.com");
 	}
 
 }
